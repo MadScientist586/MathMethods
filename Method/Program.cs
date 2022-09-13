68e65d4495e0e6952019909291128247b78c3965
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-13 12:56:38 -0500

    Did the math

---
+++ Method/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+using Method;
+Console.WriteLine("Addition:");
+Console.WriteLine(MethodClass.Add(1, 2));
+Console.WriteLine("Subtraction:");
+Console.WriteLine(MethodClass.Subtract(1, 2));
+Console.WriteLine("Multiplication:");
+Console.WriteLine(MethodClass.Multiply(1, 2));
+Console.WriteLine("Division:");
+Console.WriteLine(MethodClass.Divide(1, 2));
